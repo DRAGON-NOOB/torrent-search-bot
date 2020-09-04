a2345388b757dc37d832f844305509dc4c164c01
DRAGON-NOOB <69449962+DRAGON-NOOB@users.noreply.github.com>
2020-09-04 11:55:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ apiKey = "halyoa"
 # API KEY FOR THE API IS PUBLIC
 # DON'T ABUSE THE API
 apiParams = f"&sort=0&category=0&page=0&adult=0&key={apiKey}&concurrent=0&provider_ids[]=32"
-botToken = '<YOUR BOT API KEY HERE>'
+botToken = '1359970120:AAGTZmzhiq5xZu3ebSlgqN8Wvi2tFGN7CZ0'
 supportChatUrl = "https://t.me/mdh_hindi"
 appUrl = 'https://t.me/MaxxRider'
